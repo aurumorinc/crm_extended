c7f2e1ee9bb3e380af731305280c93f7f1fe0c2e
Aryan Singh <aquiveal@gmail.com>
2026-02-09 02:35:21 +0530

    chore(employment_type): update copyright years and timestamps
    Update copyright header in Python files and refresh creation/modified timestamps in the employment_type DocType JSON to 2026. No functional changes.

--- crm_extended/crm_extended/doctype/employment_type/employment_type.py
+++ crm_extended/crm_extended/doctype/employment_type/employment_type.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2025, hello@aurumor.com and contributors
+# Copyright (c) 2026, hello@aurumor.com and contributors
 # For license information, please see license.txt
 
 from frappe.model.document import Document
