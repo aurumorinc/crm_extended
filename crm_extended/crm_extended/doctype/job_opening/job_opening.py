729fadf5f062a98cdf14434e1e8c569d347cf38b
Aryan Singh <aquiveal@gmail.com>
2026-02-08 23:31:44 +0530

    feat(crm): add Job Opening and Employment Type doctypes
    Add new CRM Extended doctypes for recruiting:
    - Job Opening: fields for title, organization, status, employment type, location, posted/closes dates, rich description, salary block (currency, lower/upper range, salary per) and website. Includes list view flags, default values, autoname pattern (JOB-{YYYY}-{####}) and role permissions.
    - Employment Type: simple lookup doctype with unique employment_type_name and autoname from that field.
    
    Include basic Python document classes (JobOpening, EmploymentType) and package __init__ headers.

---
+++ crm_extended/crm_extended/doctype/job_opening/job_opening.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2025, hello@aurumor.com and contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+class JobOpening(Document):
+	pass
